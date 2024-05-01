b07d9115e29ac84f3e287164ef91694efcf838be
Areson251 <unicorn5596@gmail.com>
2024-05-01 19:22:17 +0400

    add kandinsky3 to test

--- scripts/inpainting_test.py
+++ scripts/inpainting_test.py
@@ -1,17 +1,21 @@
 from kandinsky3 import get_inpainting_pipeline
 import torch
 
-device_map = torch.device('cuda:0')
+device_map = torch.device('mps')
+# device_map = torch.device('cuda:0')
+print("dtype_map")
 dtype_map = {
     'unet': torch.float16,
     'text_encoder': torch.float16,
     'movq': torch.float32,
 }
 
+print("pipe")
 pipe = get_inpainting_pipeline(
     device_map, dtype_map,
 )
 
+print("image")
 image = ... # PIL Image
 mask = ... # Numpy array (HxW). Set 1 where image should be masked
 image = pipe( "A cute corgi lives in a house made out of sushi.", image, mask)
