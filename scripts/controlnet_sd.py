30bf826d7671dc151980b4ab3b253eb6a59c734b
Areson251 <unicorn5596@gmail.com>
2024-09-18 13:22:06 +0000

    add controlnet

--- scripts/controlnet_sd.py
+++ scripts/controlnet_sd.py
@@ -13,6 +13,8 @@ IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
 
 class AugmentationPipe():
     def __init__(self):
+        self.generator = torch.manual_seed(0)
+
         self.controlnet = ControlNetModel.from_pretrained(
         # "fusing/stable-diffusion-v1-5-controlnet-normal", torch_dtype=torch.float16
         "lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16
@@ -25,6 +27,7 @@ class AugmentationPipe():
 
         self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
     
+    # CONTROL_CONDITIONING_SCALE
     def __call__(self, prompt, image, num_inference_steps=20):
         return self.pipe(prompt, image, num_inference_steps=20).images[0]
 
@@ -35,11 +38,14 @@ if __name__ == "__main__":
                             default="depth", required=True)
     args = parser.parse_args()
 
-    output_folder = os.path.join(args.dimension, args.images_folder)
-    augmentation_path = os.path.join("augmented", output_folder)
+    output_folder = os.path.join(args.dimension+"_exps", args.images_folder)
+    dimension_path = os.path.join(output_folder, args.dimension)
+    augmentation_path = os.path.join(output_folder, "augmented")
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+    if not os.path.exists(dimension_path):
+        os.makedirs(dimension_path)
     if not os.path.exists(augmentation_path):
         os.makedirs(augmentation_path)
 
@@ -48,13 +54,13 @@ if __name__ == "__main__":
     images_paths = [f for f in os.listdir(args.images_folder) if f.lower().endswith(IMAGE_EXTENSIONS)]
     for image_name in tqdm.tqdm(images_paths):
         image = Image.open(os.path.join(args.images_folder, image_name))
-        ouput = Estimator.estimate(image, args.dimension)
+        estimator_ouput = Estimator.estimate(image, args.dimension)
 
         image_name = image_name.split(".")[0] + ".png"
-        ouput.save(os.path.join(output_folder, image_name))
+        estimator_ouput.save(os.path.join(dimension_path, image_name))
 
-        prompt = f"a photo of a pothole"
+        prompt = f"a photo of a snowy street scene"
         # obj = image_name.split(".")[0]
         # prompt = f"a photo of a {obj}"
-        ouput = augmentation(prompt, image)
+        ouput = augmentation(prompt, estimator_ouput)
         ouput.save(os.path.join(augmentation_path, image_name))
---
+++ scripts/utils/merge_images.py
@@ -0,0 +1,88 @@
+from PIL import Image
+import argparse
+import os
+import tqdm
+
+IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
+
+class ImageMerger():
+    def __init__(self):
+        self.images = []
+
+    def add_image(self, image):
+        self.images.append(image)
+    
+    def merge_row(self):
+        widths, heights = zip(*(i.size for i in self.images))
+        min_height = min(heights)
+        self.images, widths, heights = self.resize_images(min_height, widths, heights)
+
+        total_width = sum(widths)
+        max_height = max(heights)
+        new_image = Image.new('RGB', (total_width, max_height))
+        x_offset = 0
+        for image in tqdm.tqdm(self.images):
+            resize_koef = min_height / image.size[1]
+            resized_image = image.resize((int(image.size[0] * resize_koef), 
+                                          int(image.size[1] * resize_koef)))
+            new_image.paste(resized_image, (x_offset, 0))
+            x_offset += image.size[0]
+        return new_image
+    
+    def merge_column(self):
+        widths, heights = zip(*(i.size for i in self.images))
+
+        total_height = sum(heights)
+        max_width = max(widths)
+        new_image = Image.new('RGB', (max_width, total_height))
+        y_offset = 0
+        for image in tqdm.tqdm(self.images):
+            new_image.paste(image, (0, y_offset))
+            y_offset += image.size[1]
+        return new_image
+    
+    def resize_images(self, min_height, widths, heights):
+        new_images = []
+        new_widths = []
+        new_heights = []
+        for image, width, height in zip(self.images, widths, heights):
+            resize_koef = min_height / height
+            new_widths.append(int(width * resize_koef))
+            new_heights.append(int(height * resize_koef))
+            new_images.append(image.resize((int(width * resize_koef), 
+                                          int(height * resize_koef))))
+        return new_images, new_widths, new_heights
+    
+    def clear_images(self):
+        self.images = []
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_path", type=str, default="merged_image.png")
+    parser.add_argument("--images_folders", nargs='+', 
+                        required=True, help="Provide one or more images folders")
+    args = parser.parse_args()
+    
+    merged_rows = []
+    for folder in args.images_folders:
+        images_paths = [f for f in os.listdir(folder) if f.lower().endswith(IMAGE_EXTENSIONS)]
+        images_paths = sorted(images_paths)
+        merger = ImageMerger()
+
+        print(f"load {len(images_paths)} images from {folder}")
+        for image_path in images_paths:
+            image = Image.open(os.path.join(folder, image_path))
+            merger.add_image(image) 
+
+        print("merge images")
+        merged_rows.append(merger.merge_row()) 
+
+    merger.clear_images()
+    for image in merged_rows:
+        merger.add_image(image)
+
+    merged_image = merger.merge_column()
+    if not args.output_path.lower().endswith(IMAGE_EXTENSIONS):
+        args.output_path += ".png"
+    merged_image.save(args.output_path)
