8fa2636b834a8e28d3dee677ba5abe3defef61b9
Areson251 <unicorn5596@gmail.com>
2024-09-19 08:59:17 +0000

    fix controlnet weights load

--- scripts/controlnet_sd.py
+++ scripts/controlnet_sd.py
@@ -5,37 +5,52 @@ import tqdm
 import torch
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
 
-
 from depth.marigold import Estimator
 
 
 IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp')
+MODELS = {
+    "depth": "lllyasviel/sd-controlnet-depth",
+    "normals": "fusing/stable-diffusion-v1-5-controlnet-normal",
+}
 
 class AugmentationPipe():
-    def __init__(self):
+    def __init__(self, dimension="depth"):
         self.generator = torch.manual_seed(0)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.controlnet = ControlNetModel.from_pretrained(
-        # "fusing/stable-diffusion-v1-5-controlnet-normal", torch_dtype=torch.float16
-        "lllyasviel/sd-controlnet-depth", torch_dtype=torch.float16
+        MODELS[dimension], 
+        torch_dtype=torch.float16
         )
 
         self.pipe = StableDiffusionControlNetPipeline.from_pretrained(
         pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5", 
         controlnet=self.controlnet, safety_checker=None, torch_dtype=torch.float16,
-        ).to("cuda")
+        ).to(self.device)
 
         self.pipe.scheduler = UniPCMultistepScheduler.from_config(self.pipe.scheduler.config)
     
     # CONTROL_CONDITIONING_SCALE
-    def __call__(self, prompt, image, num_inference_steps=20):
-        return self.pipe(prompt, image, num_inference_steps=20).images[0]
+    def __call__(self, prompt, image, num_inference_steps=20,
+                 guidance_scale=7.5,
+                 controlnet_conditioning_scale=1.0,):
+
+        return self.pipe(prompt, image, 
+                         num_inference_steps=num_inference_steps,
+                         guidance_scale=guidance_scale,
+                         controlnet_conditioning_scale=controlnet_conditioning_scale,).images[0]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--images_folder", type=str, required=True)
     parser.add_argument("--dimension", type=str, choices=["depth", "normals"], 
                             default="depth", required=True)
+    parser.add_argument("--prompts_path", type=str, default="prompts/test.txt")
+    parser.add_argument("--num_steps", type=int, default=20)
+    parser.add_argument("--guidance_scale", type=float, default=7.5)
+    parser.add_argument("--controlnet_conditioning_scale", type=float, default=1.0)
+    
     args = parser.parse_args()
 
     output_folder = os.path.join(args.dimension+"_exps", args.images_folder)
@@ -50,17 +65,21 @@ if __name__ == "__main__":
         os.makedirs(augmentation_path)
 
     augmentation = AugmentationPipe()
-
-    images_paths = [f for f in os.listdir(args.images_folder) if f.lower().endswith(IMAGE_EXTENSIONS)]
-    for image_name in tqdm.tqdm(images_paths):
+    
+    with open(args.prompts_path, "r") as f:
+        prompts = f.readlines()
+    images_paths = sorted([f for f in os.listdir(args.images_folder) if f.lower().endswith(IMAGE_EXTENSIONS)])
+    
+    for idx, image_name in enumerate(tqdm.tqdm(images_paths)):
         image = Image.open(os.path.join(args.images_folder, image_name))
         estimator_ouput = Estimator.estimate(image, args.dimension)
 
         image_name = image_name.split(".")[0] + ".png"
         estimator_ouput.save(os.path.join(dimension_path, image_name))
 
-        prompt = f"a photo of a snowy street scene"
-        # obj = image_name.split(".")[0]
-        # prompt = f"a photo of a {obj}"
-        ouput = augmentation(prompt, estimator_ouput)
+        prompt = prompts[idx].split("\n")[0]
+        ouput = augmentation(prompt, estimator_ouput, 
+                             args.num_steps,
+                            args.guidance_scale,
+                            args.controlnet_conditioning_scale,)
         ouput.save(os.path.join(augmentation_path, image_name))
--- scripts/run_exps.py
+++ scripts/run_exps.py
@@ -1,18 +1,35 @@
 import subprocess
 
-arguments = [
-    # ["--images_folder", "custom_datasets/images/objects/pothole", "--dimension", "depth"],
-    # ["--images_folder", "custom_datasets/images/objects/pothole_far", "--dimension", "depth"],
-    # ["--images_folder", "custom_datasets/images/objects/random_objects", "--dimension", "depth"],
-    ["--images_folder", "custom_datasets/images/scenes/INSTSnowRoadDetection", "--dimension", "depth"],
-    # ["--images_folder", "custom_datasets/images/scenes/random_scenes", "--dimension", "depth"],
+# arguments = [
+#     # ["--images_folder", "custom_datasets/images/objects/pothole", "--dimension", "depth"],
+#     # ["--images_folder", "custom_datasets/images/objects/pothole_far", "--dimension", "depth"],
+#     # ["--images_folder", "custom_datasets/images/objects/random_objects", "--dimension", "depth"],
+#     # ["--images_folder", "custom_datasets/images/scenes/INSTSnowRoadDetection", "--dimension", "depth"],
+#     ["--images_folder", "custom_datasets/images/scenes/random_scenes", "--dimension", "depth"],
 
-    # ["--images_folder", "custom_datasets/images/objects/pothole", "--dimension", "normals"],
-    # ["--images_folder", "custom_datasets/images/objects/pothole_far", "--dimension", "normals"],
-    # ["--images_folder", "custom_datasets/images/objects/random_objects", "--dimension", "normals"],
-    ["--images_folder", "custom_datasets/images/scenes/INSTSnowRoadDetection", "--dimension", "normals"],
-    # ["--images_folder", "custom_datasets/images/scenes/random_scenes", "--dimension", "normals"],
+#     # ["--images_folder", "custom_datasets/images/objects/pothole", "--dimension", "normals"],
+#     # ["--images_folder", "custom_datasets/images/objects/pothole_far", "--dimension", "normals"],
+#     # ["--images_folder", "custom_datasets/images/objects/random_objects", "--dimension", "normals"],
+#     # ["--images_folder", "custom_datasets/images/scenes/INSTSnowRoadDetection", "--dimension", "normals"],
+#     # ["--images_folder", "custom_datasets/images/scenes/random_scenes", "--dimension", "normals"],
+# ]
+
+# for args in arguments:
+#     subprocess.run(["python", "scripts/controlnet_sd.py"] + args)
+
+arguments = [
+    ["--output_path", "results/pothole_augmented_depth.png", "--images_folders", "custom_datasets/images/objects/pothole", "depth_exps/custom_datasets/images/objects/pothole/depth", "depth_exps/custom_datasets/images/objects/pothole/augmented"],
+    ["--output_path", "results/pothole_far_augmented_depth.png", "--images_folders", "custom_datasets/images/objects/pothole_far", "depth_exps/custom_datasets/images/objects/pothole_far/depth", "depth_exps/custom_datasets/images/objects/pothole_far/augmented"],
+    ["--output_path", "results/random_objects_augmented_depth.png", "--images_folders", "custom_datasets/images/objects/random_objects", "depth_exps/custom_datasets/images/objects/random_objects/depth", "depth_exps/custom_datasets/images/objects/random_objects/augmented"],
+    ["--output_path", "results/INSTSnowRoadDetection_augmented_depth.png", "--images_folders", "custom_datasets/images/scenes/INSTSnowRoadDetection", "depth_exps/custom_datasets/images/scenes/INSTSnowRoadDetection/depth", "depth_exps/custom_datasets/images/scenes/INSTSnowRoadDetection/augmented"],
+    ["--output_path", "results/random_scenes_augmented_depth.png", "--images_folders", "custom_datasets/images/scenes/random_scenes", "depth_exps/custom_datasets/images/scenes/random_scenes/depth", "depth_exps/custom_datasets/images/scenes/random_scenes/augmented"],
+    
+    ["--output_path", "results/pothole_augmented_normals.png", "--images_folders", "custom_datasets/images/objects/pothole", "normals_exps/custom_datasets/images/objects/pothole/normals", "normals_exps/custom_datasets/images/objects/pothole/augmented"],
+    ["--output_path", "results/pothole_far_augmented_normals.png", "--images_folders", "custom_datasets/images/objects/pothole_far", "normals_exps/custom_datasets/images/objects/pothole_far/normals", "normals_exps/custom_datasets/images/objects/pothole_far/augmented"],
+    ["--output_path", "results/random_objects_augmented_normals.png", "--images_folders", "custom_datasets/images/objects/random_objects", "normals_exps/custom_datasets/images/objects/random_objects/normals", "normals_exps/custom_datasets/images/objects/random_objects/augmented"],
+    ["--output_path", "results/INSTSnowRoadDetection_augmented_normals.png", "--images_folders", "custom_datasets/images/scenes/INSTSnowRoadDetection", "normals_exps/custom_datasets/images/scenes/INSTSnowRoadDetection/normals", "normals_exps/custom_datasets/images/scenes/INSTSnowRoadDetection/augmented"],
+    ["--output_path", "results/random_scenes_augmented_normals.png", "--images_folders", "custom_datasets/images/scenes/random_scenes", "normals_exps/custom_datasets/images/scenes/random_scenes/normals", "normals_exps/custom_datasets/images/scenes/random_scenes/augmented"],
 ]
 
 for args in arguments:
-    subprocess.run(["python", "scripts/controlnet_sd.py"] + args)
+    subprocess.run(["python", "scripts/utils/merge_images.py"] + args)
